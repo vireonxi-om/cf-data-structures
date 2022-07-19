01d891457da45c47ec412a8c067b3005ea281052
rheallyc <vireonxi@gmail.com>
2022-07-19 03:47:47 +0000

    Solve 1428C: ABBB

---
+++ solutions/1428C_ABBB.cpp
@@ -0,0 +1,29 @@
+// Problem: ABBB
+// URL: https://codeforces.com/problemset/problem/1428/C
+// Rating: 1100
+// Tags: brute force, data structures, greedy, strings
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+char s[210000];
+
+void solve(){
+    scanf("%s", &s);
+    int l=strlen(s), cnt=0;
+    for(int i=l-1; i>=0; i--){
+        if(s[i]=='B') cnt++;
+        else if(cnt>0){
+            cnt--;
+            l-=2;
+        }
+    }
+    if(cnt>=2) l-=(cnt-cnt%2);
+    printf("%d\n", l);
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    for(int i=0; i<t; i++) solve();
+    return 0;
+}
