9e230a09a673d16a739b547752bd44878fda7486
rheallyc <vireonxi@gmail.com>
2023-12-01 13:37:22 +0000

    Solve 1767B: Block Towers

---
+++ solutions/1767B_Block_Towers.cpp
@@ -0,0 +1,59 @@
+// Problem: Block Towers
+// URL: https://codeforces.com/problemset/problem/1767/B
+// Rating: 800
+// Tags: data structures, greedy, sortings
+// Language: C++17 (GCC 7-32)
+#include<bits/stdc++.h>
+using namespace std;
+
+#define fi first
+#define se second
+#define mp make_pair
+#define pb push_back
+#define LC k<<1
+#define RC k<<1|1
+#define IO cin.sync_with_stdio(false); cin.tie(0); cout.tie(0);
+#define all(x) (x).begin(), (x).end()
+#define SZ(x) ((int)(x).size())
+#define rep(i,a,n) for (int i = a; i < n; i++)
+#define repn(i,a,n) for (int i = a; i <= n; i++)
+#define per(i,a,n) for (int i = n - 1; i >= a; i--)
+#define pern(i,a,n) for (int i = n; i >= a; i--)
+
+typedef long long LL;
+typedef unsigned long long ull;
+typedef pair<int, int> PII;
+typedef pair<double, double> PDD;
+
+const int N = 210000;
+const int M = 11000;
+const int mod = 1000000007;
+const int inf = (int)1e9;
+const double eps = 1e-10;
+const long long INF = (long long)1e18;
+
+int n, num, a[N];
+
+void solve() {
+	cin >> n;
+	cin >> num;
+	rep(i, 1, n) cin >> a[i];
+	sort(a + 1, a + n);
+	rep(i, 1, n) {
+		if (a[i] <= num) continue;
+		num += (a[i] - num + 1) / 2; 
+	}
+	cout << num << "\n";
+}
+
+int main() {
+    IO;
+    int t;
+    cin >> t;
+    repn(i, 1, t) solve();
+    return 0;
+}
+
+
+
+
